0a808b9c186e3f5b3ac7ecaaf43611f6bf89a434
Anirudha Bose <anirudha.bose@alumni.cern>
2026-07-14 22:16:06 +0530

    ykding 0.4.0

--- Formula/ykding.rb
+++ Formula/ykding.rb
@@ -1,8 +1,8 @@
 class Ykding < Formula
   desc "Sound and notification when your YubiKey waits for a touch"
   homepage "https://github.com/onyb/ykding"
-  url "https://github.com/onyb/ykding/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "65678dd8ad7bc7d3cb370dd8f32d7c0ef5eae900c8e182712961f353faca340d"
+  url "https://github.com/onyb/ykding/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "7a8612a36526448e160067e38985fb1fe9eea5b98eadf84082aa17336d897c31"
   license "MIT"
 
   def install
